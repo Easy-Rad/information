e687421717eecb4a5badba4688ff2d2826154dcb
Edward Ganly <11688686+zlodag@users.noreply.github.com>
2025-06-16 13:52:29 +0000

    elm-ui

--- src/Main.elm
+++ src/Main.elm
@@ -3,18 +3,17 @@ module Main exposing (..)
 import Browser
 import Browser.Navigation as Nav
 import Dict exposing (Dict)
-import Element exposing (Column, Element, IndexedColumn, alignRight, centerY, column, el, fill, indexedTable, layout, link, padding, rgb255, row, spacing, table, text, width)
+import Element exposing (Column, Element, centerY, column, el, fill, height, layout, link, mouseOver, padding, paddingXY, rgb255, row, shrink, spacing, table, text, width)
+import Element.Background as Background
 import Element.Border as Border
-import Element.Font as Font
-import Element.Input exposing (button)
-import Html exposing (a)
+import Element.Font as Font exposing (center)
 import Http
 import Json.Decode exposing (Decoder)
-import List exposing (head, range)
-import Maybe exposing (withDefault)
-import Url exposing (Protocol(..))
-import Url.Builder
-import Url.Parser exposing ((</>), Parser, fragment, oneOf, s, string, top)
+import List exposing (range)
+import Maybe
+import Tuple exposing (first, second)
+import Url
+import Url.Parser exposing ((</>), Parser, oneOf, s, string, top)
 
 
 
@@ -120,8 +119,8 @@ type Data
     | Loading
     | LoadedBaseRosterUsers (List UserAssignment)
     | LoadedBaseRosterShifts (List Shift)
-    | LoadedBaseRosterUser User BaseRosterUser
-    | LoadedBaseRosterShift Shift BaseRosterShift
+    | LoadedBaseRosterUser BaseRosterUser
+    | LoadedBaseRosterShift BaseRosterShift
 
 
 type alias BaseRosterUser =
@@ -146,8 +145,8 @@ type Msg
     | UrlChanged Url.Url
     | GotBaseRosterUsers (Result Http.Error (List UserAssignment))
     | GotBaseRosterShifts (Result Http.Error (List Shift))
-    | GotBaseRosterUser User (Result Http.Error BaseRosterUser)
-    | GotBaseRosterShift Shift (Result Http.Error BaseRosterShift)
+    | GotBaseRosterUser (Result Http.Error BaseRosterUser)
+    | GotBaseRosterShift (Result Http.Error BaseRosterShift)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -179,12 +178,12 @@ update msg model =
             , Cmd.none
             )
 
-        GotBaseRosterUser user result ->
+        GotBaseRosterUser result ->
             ( { model
                 | data =
                     case result of
                         Ok base_roster ->
-                            LoadedBaseRosterUser user base_roster
+                            LoadedBaseRosterUser base_roster
 
                         Err _ ->
                             Failure
@@ -192,12 +191,12 @@ update msg model =
             , Cmd.none
             )
 
-        GotBaseRosterShift shift result ->
+        GotBaseRosterShift result ->
             ( { model
                 | data =
                     case result of
                         Ok base_roster ->
-                            LoadedBaseRosterShift shift base_roster
+                            LoadedBaseRosterShift base_roster
 
                         Err _ ->
                             Failure
@@ -232,8 +231,42 @@ subscriptions _ =
 
 anchor : List (Element.Attribute msg)
 anchor =
-    [ Font.color (rgb255 0 0 255)
-    , Font.underline
+    [ Font.size 16
+    , paddingXY 8 4
+    , Border.rounded 4
+    , width fill
+    , mouseOver mouseOverAttrs
+    ]
+        ++ defaultAttrs
+
+
+color =
+    { blue = rgb255 0x72 0x9F 0xCF
+    , purple = rgb255 0x00 0x14 0xFF
+    , yellow = rgb255 0xFC 0xFF 0x38
+    , darkCharcoal = rgb255 0x2E 0x34 0x36
+    , darkBlue = rgb255 0x37 0x7F 0xC9
+    , lightBlue = rgb255 0xC5 0xE8 0xF7
+    , lightGrey = rgb255 0xE0 0xE0 0xE0
+    , white = rgb255 0xFF 0xFF 0xFF
+    }
+
+
+defaultAttrs =
+    [ Background.color color.lightBlue
+    , Font.color color.darkCharcoal
+    ]
+
+
+activeAttrs =
+    [ Background.color color.darkCharcoal
+    , Font.color color.white
+    ]
+
+
+mouseOverAttrs =
+    [ Background.color color.yellow
+    , Font.color color.darkCharcoal
     ]
 
 
@@ -241,28 +274,115 @@ view : Model -> Browser.Document Msg
 view model =
     { title = "Information"
     , body =
-        [ column [ spacing 10 ]
-            [ row [ spacing 10 ]
-                [ link anchor { url = "/", label = text "Home" }
-                , link anchor { url = "/base_roster", label = text "Base roster" }
-                ]
-            , viewBaseRoster model
+        [ column
+            [ spacing 10
+            , width fill
             ]
-            |> layout [ padding 10 ]
+            [ Url.Parser.parse routeParser model.url |> viewNavBar
+            , viewBaseRoster model |> el [ padding 8 ]
+            ]
+            |> layout []
         ]
     }
+
+
+navBarLink : String -> String -> Bool -> Element msg
+navBarLink url label active =
+    link
+        ([ Font.size 20
+         , paddingXY 8 4
+         , Border.rounded 4
+         , width shrink
+         , mouseOver mouseOverAttrs
+         ]
+            ++ (if active then
+                    activeAttrs
+
+                else
+                    defaultAttrs
+               )
+        )
+        { url = url
+        , label = text label
+        }
+
+
+viewNavBar : Maybe Route -> Element msg
+viewNavBar route =
+    let
+        baseRosterLink =
+            navBarLink "/base_roster" "Base roster"
+
+        baseRosterUsersLink =
+            navBarLink "/base_roster/users" "Users"
+
+        baseRosterShiftsLink =
+            navBarLink "/base_roster/shifts" "Shifts"
+
+        links : List (Bool -> Element msg)
+        links =
+            navBarLink "/" "Home"
+                :: (case route of
+                        Just BaseRosterHomeRoute ->
+                            [ baseRosterLink
+                            ]
+
+                        Just BaseRosterUsersRoute ->
+                            [ baseRosterLink
+                            , baseRosterUsersLink
+                            ]
+
+                        Just BaseRosterShiftsRoute ->
+                            [ baseRosterLink
+                            , baseRosterShiftsLink
+                            ]
+
+                        Just (BaseRosterUserRoute user) ->
+                            [ baseRosterLink
+                            , baseRosterUsersLink
+                            , navBarLink ("/base_roster/user/" ++ user) user
+                            ]
+
+                        Just (BaseRosterShiftRoute shift) ->
+                            [ baseRosterLink
+                            , baseRosterShiftsLink
+                            , navBarLink ("/base_roster/shift/" ++ shift) (percentDecode shift)
+                            ]
+
+                        _ ->
+                            []
+                   )
+    in
+    row
+        [ width fill
+        , padding 8
+        , spacing 8
+        , Background.color color.blue
+        ]
+    <|
+        List.indexedMap (\index link -> link (index == List.length links - 1)) links
+
+
+viewSubMenu : List ( String, String ) -> Element msg
+viewSubMenu items =
+    column [ spacing 4 ]
+        (items
+            |> List.map (\( url, label ) -> link anchor { url = url, label = text label })
+        )
 
 
 viewBaseRoster : Model -> Element msg
 viewBaseRoster model =
     case model.data of
         Home ->
-            text "Information lies here."
+            viewSubMenu
+                [ ( "/base_roster", "Base Roster" )
+                ]
 
         BaseRosterHome ->
-            row [ spacing 10 ]
-                [ link anchor { url = "/base_roster/users", label = text "Users" }
-                , link anchor { url = "/base_roster/shifts", label = text "Shifts" }
+            viewSubMenu
+                [ ( "/base_roster/users", "Users" )
+                , ( "/base_roster/shifts", "Shifts" )
                 ]
 
         Failure ->
@@ -272,41 +392,28 @@ viewBaseRoster model =
             text "Loading..."
 
         LoadedBaseRosterUsers users ->
-            viewList "Users" users viewItemUser
+            users |> List.map transformUser |> viewSubMenu
 
         LoadedBaseRosterShifts shifts ->
-            viewList "Shifts" shifts viewItemShift
+            shifts |> List.map transformShift |> viewSubMenu
 
-        LoadedBaseRosterUser user base_roster_user ->
-            viewRosterGrid ("User: " ++ user) (viewCell base_roster_user viewItemShift)
+        LoadedBaseRosterUser base_roster_user ->
+            viewCell base_roster_user transformShift |> viewRosterGrid
 
-        LoadedBaseRosterShift shift base_roster_shift ->
-            viewRosterGrid ("Shift: " ++ shift) (viewCell base_roster_shift viewItemUser)
-
-
-viewList : String -> List a -> (a -> Element msg) -> Element msg
-viewList title items transform =
-    column []
-        [ text title
-        , items
-            |> List.map transform
-            |> column []
-        ]
+        LoadedBaseRosterShift base_roster_shift ->
+            viewCell base_roster_shift transformUser |> viewRosterGrid
 
 
-viewRosterGrid : String -> (Int -> Int -> Element msg) -> Element msg
-viewRosterGrid title transform =
+viewRosterGrid : (Int -> Int -> Element msg) -> Element msg
+viewRosterGrid transform =
     let
-        headerAttrs : List (Element.Attribute msg)
-        headerAttrs =
+        headerBaseAttrs : List (Element.Attribute msg)
+        headerBaseAttrs =
             [ Font.bold
-            , Font.center
-
-            -- , Border.color (rgb255 0 0 0)
-            , Border.width 1
+            , padding 8
             ]
 
-        columns : List (IndexedColumn Int msg)
+        columns : List (Column Int msg)
         columns =
             [ "Monday"
             , "Tuesday"
@@ -314,33 +421,43 @@ viewRosterGrid title transform =
             , "Thursday"
             , "Friday"
             ]
-                |> List.map (\dayName -> IndexedColumn (text dayName |> el headerAttrs) fill transform)
+                |> List.indexedMap (\dayIndex dayName -> transform dayIndex |> Column (text dayName |> el (center :: headerBaseAttrs)) fill)
+                |> (::) (Column Element.none shrink (\week -> text ("Week " ++ String.fromInt (week + 1)) |> el (centerY :: headerBaseAttrs)))
     in
-    column [ spacing 10 ]
-        [ text title
-        , indexedTable [ Border.width 1 ]
-            { data = range 0 3
-            , columns = columns
-            }
-        ]
+    table []
+        { data = range 0 3
+        , columns = columns
+        }
 
 
-viewCell : Dict String (List a) -> (a -> Element msg) -> Int -> Int -> Element msg
+viewCell : Dict String (List a) -> (a -> ( String, String )) -> Int -> Int -> Element msg
 viewCell base_roster transform dayIndex weekIndex =
     Dict.get (weekIndex * 7 + (dayIndex + 1) |> String.fromInt) base_roster
         |> Maybe.withDefault []
-        |> List.map transform
-        |> column [ padding 10, Border.width 1 ]
+        |> List.map (transform >> (\a -> link anchor { url = first a, label = second a |> text }))
+        |> column
+            [ spacing 4
+            , padding 8
+            , width fill
+            , height fill
+            , Background.color
+                (if modBy 2 weekIndex == 0 then
+                    color.darkCharcoal
+
+                 else
+                    color.blue
+                )
+            ]
 
 
-viewItemShift : String -> Element msg
-viewItemShift shift =
-    link anchor { url = "/base_roster/shift/" ++ shift, label = text shift }
+transformShift : Shift -> ( String, String )
+transformShift shift =
+    ( "/base_roster/shift/" ++ shift, shift )
 
 
-viewItemUser : UserAssignment -> Element msg
-viewItemUser user =
-    link anchor { url = "/base_roster/user/" ++ user.user, label = user.lastname ++ ", " ++ user.firstname |> text }
+transformUser : UserAssignment -> ( String, String )
+transformUser user =
+    ( "/base_roster/user/" ++ user.user, user.lastname ++ ", " ++ user.firstname )
 
 
 
@@ -372,7 +489,7 @@ getBaseRosterUser : User -> Cmd Msg
 getBaseRosterUser user =
     Http.get
         { url = baseRosterUrl ++ "/base_roster/user/" ++ user
-        , expect = Http.expectJson (GotBaseRosterUser user) baseRosterUserDecoder
+        , expect = Http.expectJson GotBaseRosterUser baseRosterUserDecoder
         }
 
 
@@ -380,8 +497,13 @@ getBaseRosterShift : Shift -> Cmd Msg
 getBaseRosterShift shift =
     Http.get
         { url = baseRosterUrl ++ "/base_roster/shift/" ++ shift
-        , expect = Http.expectJson (Url.percentDecode shift |> Maybe.withDefault shift |> GotBaseRosterShift) baseRosterShiftDecoder
+        , expect = Http.expectJson GotBaseRosterShift baseRosterShiftDecoder
         }
+
+
+percentDecode : String -> String
+percentDecode str =
+    Url.percentDecode str |> Maybe.withDefault str
 
 
 baseRosterUserDecoder : Decoder BaseRosterUser
